7ed54924ecebb97e968aa4c101b6979b23fd8c95
Daniela Castro <daniela.castro@phs.mx>
2023-05-30 11:30:19 -0600

    creacion masiva de docentes

--- src/controllers/docente.controller.js
+++ src/controllers/docente.controller.js
@@ -32,39 +32,62 @@ exports.getDocentes = async (req, res) => {
 };
 
 exports.crudDocente = async (req, res) => {
-    try {
-        let docente = req.body; // Guarda los datos del docente en la variable
+    let docentes = req.body;
+    const docentesAsync = async (docente) => {
+        try {
+            // let docente = req.body; // Guarda los datos del docente en la variable
 
-        if (docente.docenteId == null) { // En caso de que el id sea nulo, se crea un nuevo docente.
-            let newDocente = await Docente.create(docente);
-            if (newDocente) {
-                return res.status(200).json({
-                    success: true,
-                    message: "Se ha guardado el docente",
-                });
-            }
-        } else if (docente.docenteId) { // En caso de que el id NO sea nulo, se actualiza el docente.
-            let docenteId = docente.docenteId;
-            delete docente.docenteId;
+            if (docente.docenteId == null) { // En caso de que el id sea nulo, se crea un nuevo docente.
+                let newDocente = await Docente.create(docente);
+                if (newDocente) {
 
-            let updatedDocente = await Docente.update(docente, {
-                where: {
-                    docenteId: docenteId
+                    let data = {
+                        docenteId: newDocente.id,
+                        carreraId: docente.carreraId,
+                        status: 1
+                    }
+
+                    let carreraDocente = await CarreraDocente.create(data);
+
                 }
-            });
-            if (updatedDocente) {
-                return res.status(200).json({
-                    success: true,
-                    message: "Se ha guardado el docente.",
+            } else if (docente.docenteId) { // En caso de que el id NO sea nulo, se actualiza el docente.
+                let docenteId = docente.docenteId;
+                delete docente.docenteId;
+
+                let updatedDocente = await Docente.update(docente, {
+                    where: {
+                        docenteId: docenteId
+                    }
                 });
             }
+        } catch (e) {
+            console.log(e);
+            return res.status(500).json({
+                success: false,
+                message: "Ha ocurrido un error al guardar el registro.",
+                error: e.message,
+            });
         }
-    } catch (e) {
-        console.log(e);
-        return res.status(500).json({
-            success: false,
-            message: "Ha ocurrido un error al guardar el registro.",
-            error: e.message,
-        });
+    }
+    if (docentes != undefined) {
+        return Promise.all(docentes.map(docentesAsync)) // Mapeo de los docentes, se aplica la función anterior
+            .then(result => {
+                return [{
+                    success: true,
+                    message: `Se guardó correctamente los docentes.`,
+                }];
+            }).catch(error => {
+                console.log(error)
+                return [{
+                    success: false,
+                    message: `Ha ocurrido un error al guardar los docentes.`,
+                    error: error
+                }]
+            })
+    } else { // En caso de que no existan datos, se retorna el mensaje.
+        return [{
+            success: true,
+            message: `No existen docentes.`,
+        }];
     }
 };
