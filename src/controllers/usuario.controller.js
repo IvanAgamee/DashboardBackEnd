8bc7cc143cb9e80ac6af81e52181a4b68cbb1198
Daniela Castro <daniela.castro@phs.mx>
2023-09-11 20:11:24 -0600

    imagen user webp

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -143,6 +143,8 @@ exports.crudUser = async (req, res) => {
                     message: 'Nombre de usuario ya existente'
                 });
             } else {  // En caso de que no exista o no se repita el nombre de usuario
+                usuario.urlImagen = usuario.urlImagen ? usuario.urlImagen : 'user.webp';
+
                 let newUsuario = await Usuario.create(usuario); // crea el usuario
                 if (newUsuario) {
                     return res.status(200).json({
