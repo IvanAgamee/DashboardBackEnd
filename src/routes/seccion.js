0b288bc272c1d84e7aeef7fc320c6c0c64746e4c
Daniela Castro <daniela.castro@phs.mx>
2023-10-02 11:12:46 -0600

    Squashed commit of the following:
    commit 404057e5932953e24f742d2daa7a00da538af4f4
    Merge: 440c88c cdcd1f4
    Author: Daniela Castro <daniela.castro@phs.mx>
    Date:   Mon Oct 2 11:12:16 2023 -0600
    
        Merge branch 'main' into branch-ericmontalvo
    
    commit 440c88c1c92d47ca30025fd9302ac2e68d970bdf
    Merge: e3b5955 7d0c06f
    Author: Daniela Castro <daniela.castro@phs.mx>
    Date:   Mon Oct 2 01:49:26 2023 -0600
    
        Merge branch 'main' into branch-ericmontalvo
    
    commit e3b5955ae86837ae9d9c36c3a26afcbd66b1a5b5
    Author: Eric M.M.C <eric.montalvo@protonmail.com>
    Date:   Sun Oct 1 22:53:34 2023 -0600
    
        endpoints para obtener ultima seccion y borrado de objetos
    
    commit 32f90d4a2dc0056f31fa786e20fb8846809f24fa
    Merge: 23bd90f d02225f
    Author: Eric M.M.C <eric.montalvo@protonmail.com>
    Date:   Sun Oct 1 10:36:30 2023 -0600
    
        Merge branch 'main' of https://github.com/IvanAgamee/DashboardBackEnd into branch-ericmontalvo
    
    commit 23bd90f8cc2d7a7aee2ce39dc291d7e9ae79342e
    Author: Eric M.M.C <eric.montalvo@protonmail.com>
    Date:   Sun Sep 24 20:04:39 2023 -0600
    
        Endpoint de obtener el ultimo ID en creación sección
    
    commit d7e6e45f4dac3802359441be8fed66099e1d3a6c
    Merge: c68ca33 5c22825
    Author: Eric M.M.C <eric.montalvo@protonmail.com>
    Date:   Tue Sep 19 20:32:03 2023 -0600
    
        arreglo de docentes
    
    commit c68ca33a692229f3230947049d38728fc2bb106d
    Author: Eric M.M.C <eric.montalvo@protonmail.com>
    Date:   Tue Sep 19 20:30:20 2023 -0600
    
        endpoints de secciones
    
    commit 46d0d83aa089c737c33d7d1b65593b41535e1ed9
    Merge: e070f7a 65dbfff
    Author: Eric M.M.C <eric.montalvo@protonmail.com>
    Date:   Sun Sep 17 21:41:25 2023 -0600
    
        Merge branch 'branch-ericmontalvo' of https://github.com/IvanAgamee/DashboardBackEnd into branch-ericmontalvo
    
    commit 65dbfff8851b1816a551956c5471616738b9c8fb
    Merge: d7694f3 3e39483
    Author: Daniela Castro <daniela.castro@phs.mx>
    Date:   Sun Sep 17 21:39:27 2023 -0600
    
        Merge branch 'main' into branch-ericmontalvo
    
    commit e070f7a0c30785397a5fe0793d550e315cf4a668
    Merge: a725b8f 3e39483
    Author: Eric M.M.C <eric.montalvo@protonmail.com>
    Date:   Sun Sep 17 21:10:22 2023 -0600
    
        Merge branch 'main' of https://github.com/IvanAgamee/DashboardBackEnd into branch-ericmontalvo
    
        # Conflicts:
        #	src/controllers/docente.controller.js
        #	src/models/Docente.js
    
    commit a725b8f0edcc3ac3b3e1ef27b89da7b0bc548dde
    Author: Eric M.M.C <eric.montalvo@protonmail.com>
    Date:   Sun Sep 17 20:50:13 2023 -0600
    
        arreglo de docentes
    
    commit d7694f3119cc01736fa27dba3dba061d37e44d05
    Author: Eric M.M.C <eric.montalvo@protonmail.com>
    Date:   Sun Sep 17 16:19:33 2023 -0600
    
        refactorización de relaciones en ProgramaEstudio y Docente

--- src/routes/seccion.js
+++ src/routes/seccion.js
@@ -2,6 +2,8 @@ module.exports = app => {
     var router = require('express').Router();
     var m = require("../controllers/seccion.controller");
 
+    router.get('/getSeccionById', m.getSeccionById);
+    router.get('/getLastIdSecccion',m.getLastIdSecccion);
     router.get('/getSeccionByProgramaId', m.getSeccionByProgramaId);
     router.get('/getObjetoByProgramaId', m.getObjetoByProgramaId);
     router.get('/getObjetoBySeccionId', m.getObjetoBySeccionId);
@@ -15,6 +17,7 @@ module.exports = app => {
     router.post('/crudObjeto', m.crudObjeto);
     router.post('/crudSeccionMasivo', m.crudSeccionMasivo);
     router.post('/crudObjetoMasivo', m.crudObjetoMasivo);
+    router.delete('/borrarObjetos',m.borrarObjetosById);
 
     app.use('/api/seccion', router);
 }
